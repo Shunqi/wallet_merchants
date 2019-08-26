75fceb204a28baa2843d246fdd13de0e81fcb226
Shunqi Wang <wangshunqi2@gmail.com>
2019-08-25 22:10:23 -0400

    Register interceptor

--- src/main/java/com/shunqi/wallet/merchants/MerchantsApplication.java
+++ src/main/java/com/shunqi/wallet/merchants/MerchantsApplication.java
@@ -1,13 +1,27 @@
 package com.shunqi.wallet.merchants;
 
+import com.shunqi.wallet.merchants.security.RequestHeaderInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(MerchantsApplication.class, args);
+    }
+
+    @Resource
+    private RequestHeaderInterceptor authCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor)
+                .addPathPatterns("/merchants/**");
     }
 
 }
